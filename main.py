0686e31fe7a4ff04c864a09d12c6b6ba580f151c
Sean Kerry <spkerry@live.com>
2017-04-09 18:58:57 -0500

    created all templates; created blog and newpost handlers; set up datastore

--- main.py
+++ main.py
@@ -1,25 +1,63 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+import os
 import webapp2
+import jinja2
 
-class MainHandler(webapp2.RequestHandler):
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
+
+class Blog(db.Model):
+    title = db.StringProperty(required = True)
+    body = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class MainPage(Handler):
     def get(self):
-        self.response.write('Hello world!')
+        self.render("main-page.html")
+
+#class AddPost(Handler):
+
+
+class NewPost(Handler):
+    def get(self, title="", body="", error=""):
+        self.render("new-post.html", title=title, body=body, error=error)
+
+    def post(self):
+        title = self.request.get("title")
+        body = self.request.get("body")
+
+        if body and title:
+            b = Blog(title = title, body = body)
+            b.put()
+
+            self.redirect('/blog')
+        else:
+            error = "we need both a title and your blog!"
+            self.render("new-post.html", title=title, body=body, error=error)
+
+class BlogPage(Handler):
+    def get(self, title="", body=""):
+        blogs = db.GqlQuery("SELECT * FROM Blog "
+                           "ORDER BY created DESC "
+                           "LIMIT 5 ")
+
+        self.render("blog.html", title=title, body=body, blogs=blogs)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainPage),
+    ('/blog', BlogPage),
+    ('/newpost', NewPost)
 ], debug=True)
